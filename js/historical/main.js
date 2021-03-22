032b33dfd0404d55929d072b24795d58325b8440
Edward <edward.le@alumni.ubc.ca>
2021-03-22 13:37:20 -0700

    Added some exta formatting in main.js for the data.
    Filtered data in co2/gdp to better reflect data availability.

--- js/historical/main.js
+++ js/historical/main.js
@@ -1,16 +1,22 @@
 /**
  * Load data from CSV file asynchronously and render charts
  */
+let uuid = 0;
 d3.csv('data/historical/historical_dataset.csv').then(data => {
   console.log('historical data', data)
   // Convert columns to numerical values
   data.forEach(d => {
     Object.keys(d).forEach(attr => {
+      d.UUID = uuid;
+      d.Year = +d.Year;
+      d.CO2eq = +d.CO2eq;
+      d.GDP = +d.GDP;
+      d.Population = +d.Population;
+      d.CO2eq_tn_per_person = +d.CO2eq_tn_per_person;
+      d.CO2eq_tn_per_mil$GDP = +d.CO2eq_tn_per_mil$GDP;
+      uuid++;
     });
   });
-
-  data.sort((a,b) => a.label - b.label);
-
 });
 
 /*
